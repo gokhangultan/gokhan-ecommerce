25d1a174c7a0f7fc3bc48915413ce67da6ea3761
gokhangultan <gultangokhan@gmail.com>
2024-02-13 15:29:58 +0300

    home hero page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,31 +1,38 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { UncontrolledCarousel } from 'reactstrap'; // Import UncontrolledCarousel from Reactstrap
+import { UncontrolledCarousel } from 'reactstrap'; 
 
-function IndividualIntervalsExample() {
+function CustomCarousel() {
   return (
-    <UncontrolledCarousel
-      items={[
-        {
-          altText: 'Slide 1',
-          caption: 'Slide 1',
-          key: 1,
-          src: 'https://picsum.photos/id/123/1200/600'
-        },
-        {
-          altText: 'Slide 2',
-          caption: 'Slide 2',
-          key: 2,
-          src: 'https://picsum.photos/id/456/1200/600'
-        },
-        {
-          altText: 'Slide 3',
-          caption: 'Slide 3',
-          key: 3,
-          src: 'https://picsum.photos/id/678/1200/600'
-        }
-      ]}
-    />
+    <div>
+      
+
+      <div style={{ float: 'left' }}>
+        <UncontrolledCarousel
+          items={[
+            {
+              altText: 'Slide 1',
+              caption: 'Slide 1',
+              key: 1,
+              src: 'https://picsum.photos/id/123/1200/600'
+            },
+            {
+              altText: 'Slide 2',
+              caption: 'Slide 2',
+              key: 2,
+              src: 'https://picsum.photos/id/456/1200/600'
+            },
+            {
+              altText: 'Slide 3',
+              caption: 'Slide 3',
+              key: 3,
+              src: 'https://picsum.photos/id/678/1200/600'
+            }
+          ]}
+        />
+      </div>
+    </div>
   );
 }
 
-export default IndividualIntervalsExample;
+export default CustomCarousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import Carousel from "../components/Carousel"
+import Carousel from "../components/Carousel";
 
 export default function Home() {
     return (
         <div>
-        <div><Carousel /></div>
+            <div className="relative text-white">
+                <Carousel />
+
+                <div className="absolute top-0 left-0 z-10 flex flex-col text-left py-[40px] gap-[24px] ml-20">
+                    <div><h5 className="font-bold text-[16px] font-container leading-[24px]">SUMMER 2020</h5></div>
+                    <div>
+                        <h1 className="font-bold text-[58px] font-container leading-[80px]">Multicoloured </h1>
+                        <br/>
+                        <h1 className="font-bold text-[58px] font-container leading-[80px]">Tie-dye Sweater </h1>
+                    </div>
+                    <div><h4 className="text-[20px] leading-[30px] font-container">We know how large objects will act,</h4></div>
+                    <button className="text-center border-1 w-[163px] h-[52px]">SHOP NOW</button>
+                </div>
+            </div>
         </div>
     )
 }
